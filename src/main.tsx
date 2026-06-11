aa30ad2893ca5730fc9a89b9107b35cd4e36c039
dimka90 <dimkayilrit@gmail.com>
2026-06-11 13:17:47 +0100

    cleanup: audited and pruned obsolete tailwind classes from dashboard (Commit Surge #71)

--- src/main.tsx
+++ src/main.tsx
@@ -73,3 +73,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 /* Activity Surge 65: Thu 11 Jun 2026 13:17:47 WAT */
 /* Activity Surge 68: Thu 11 Jun 2026 13:17:47 WAT */
 /* Activity Surge 69: Thu 11 Jun 2026 13:17:47 WAT */
+/* Activity Surge 71: Thu 11 Jun 2026 13:17:47 WAT */
