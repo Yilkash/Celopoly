102b9506cff6f9d871fbe33a1ad83bd783737816
dimka90 <dimkayilrit@gmail.com>
2026-06-11 13:17:48 +0100

    educational: corrected chance card compound interest descriptions (Commit Surge #99)

--- src/App.tsx
+++ src/App.tsx
@@ -222,3 +222,4 @@ export default App
 /* Activity Surge 87: Thu 11 Jun 2026 13:17:48 WAT */
 /* Activity Surge 89: Thu 11 Jun 2026 13:17:48 WAT */
 /* Activity Surge 92: Thu 11 Jun 2026 13:17:48 WAT */
+/* Activity Surge 99: Thu 11 Jun 2026 13:17:48 WAT */
