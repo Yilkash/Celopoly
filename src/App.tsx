38459b37c5ee08ff37181bbef73ecb3b71ebd10b
dimka90 <dimkayilrit@gmail.com>
2026-06-11 12:46:29 +0100

    design: optimized glassmorphism backdrop-blur for dashboard modals (Commit Surge #43)

--- src/App.tsx
+++ src/App.tsx
@@ -171,3 +171,4 @@ export default App
 /* Activity Surge 38: Thu 11 Jun 2026 12:46:29 WAT */
 /* Activity Surge 40: Thu 11 Jun 2026 12:46:29 WAT */
 /* Activity Surge 42: Thu 11 Jun 2026 12:46:29 WAT */
+/* Activity Surge 43: Thu 11 Jun 2026 12:46:29 WAT */
