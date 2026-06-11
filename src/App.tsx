34ad8c49d877a47f5f695c1fdfe30ef5999cdff9
dimka90 <dimkayilrit@gmail.com>
2026-06-11 12:46:29 +0100

    refactor: transitioned game turn validation logic to Zustand hooks (Commit Surge #28)

--- src/App.tsx
+++ src/App.tsx
@@ -164,3 +164,4 @@ export default App
 /* Activity Surge 20: Thu 11 Jun 2026 12:46:29 WAT */
 /* Activity Surge 24: Thu 11 Jun 2026 12:46:29 WAT */
 /* Activity Surge 26: Thu 11 Jun 2026 12:46:29 WAT */
+/* Activity Surge 28: Thu 11 Jun 2026 12:46:29 WAT */
