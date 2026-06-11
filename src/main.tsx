11fecea8cf1e07a2f5d9cdb6a407133d28a8de1a
dimka90 <dimkayilrit@gmail.com>
2026-06-11 12:46:30 +0100

    refactor: transitioned game turn validation logic to Zustand hooks (Commit Surge #77)

--- src/main.tsx
+++ src/main.tsx
@@ -40,3 +40,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 /* Activity Surge 66: Thu 11 Jun 2026 12:46:30 WAT */
 /* Activity Surge 69: Thu 11 Jun 2026 12:46:30 WAT */
 /* Activity Surge 71: Thu 11 Jun 2026 12:46:30 WAT */
+/* Activity Surge 77: Thu 11 Jun 2026 12:46:30 WAT */
